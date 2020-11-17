ba80d040a2b98878e4f53e7dbdea03be07c912a2
Amin <31763634+Amin-Matola@users.noreply.github.com>
2020-11-17 16:38:59 +0200

    Create Config.java

---
+++ JavaMysql/databases/Config.java
@@ -0,0 +1,11 @@
+package JavaMysql.databases;
+
+public class Config{
+    public static String
+
+            HOST        = "localhost",
+            DB          = "database",
+            USERNAME    = "root",
+            PASSWD      = "",
+            TABLE       = "";
+}
