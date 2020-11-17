25c0bdad01cc11ba9c75049e43d6c67a70a90feb
Amin <31763634+Amin-Matola@users.noreply.github.com>
2020-11-17 10:06:23 +0200

    Create mail.php

---
+++ PhpMysql/databases/example/emails/mail.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h1>Hello There</h1>
+        <p>This is a sample email.</p>
+        <p>It can be changed as much as you wish</p>
+    </body>
+</html>
