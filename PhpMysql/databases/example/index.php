2953c8da8ef637114767bd26b29bfdf515d5ee94
Amin <31763634+Amin-Matola@users.noreply.github.com>
2020-11-17 09:58:45 +0200

    Create index.php

---
+++ PhpMysql/databases/example/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Forms</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<script type="text/javascript" src="assets/jquery.min.js"></script>
+</head>
+<body>
+	<h1>Form Manipulations</h1>
+	<form name="first-form" method="post" action='actions.php'>
+		<legend>Form Name Here</legend>
+		<input type="url" name="website" placeholder="Website url..." class="first-form" required="1"/><br/>
+		<input type="email" name="email" placeholder="user@example.com" class="first-form" required="1"/><br/>
+		<input type="tel" name="phone" placeholder="+265881014250" class="second-form"/><br/>
+		<input type="text" name="name" placeholder="Your name" class="second-form"/><br/>
+		<input type='hidden' name='nounce' value="^ahqbhbhew09*721"/>
+		<input type="submit" value="Submit"/>
+	</form>
+
+<script type="text/javascript" src="assets/main.js"></script>
+</body>
+</html>
