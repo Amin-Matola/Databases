dc97a5ad6eff799b89e8cfd7fffac4b7b5b6a30b
Amin <31763634+Amin-Matola@users.noreply.github.com>
2021-02-05 15:44:11 +0200

    Update Config.java

--- JavaMysql/databases/Config.java
+++ JavaMysql/databases/Config.java
@@ -1,3 +1,11 @@
+/* ****************************
+ * Main Table class, handling Tables
+ * This class derives from Database Abstract Class
+ *
+ * Author:          AMIN MATOLA
+ * Date Modified:   17 Nov 2020
+ */
+
 package JavaMysql.databases;
 
 public class Config{
